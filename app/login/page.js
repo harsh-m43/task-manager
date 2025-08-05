a101de3f8a53bc560bbab83a06671aa98fe51dbc
Harsh Mahale <mahale22bec43@iiitkottayam.ac.in>
2025-08-06 02:58:53 +0530

    Initial commit

--- app/login/page.js
+++ app/login/page.js
@@ -1,13 +1,12 @@
 "use client";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import LoginForm from '../../components/LoginForm';  // Make sure this is also client component if it uses hooks
+
+import LoginForm from '../../components/LoginForm';
 import { isAuthenticated } from '../../utils/auth';
 
 export default function Login() {
   const router = useRouter();
-  
+
   useEffect(() => {
     if (typeof window !== 'undefined' && isAuthenticated()) {
       router.push('/tasks');
--- app/tasks/page.js
+++ app/tasks/page.js
@@ -2,36 +2,28 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-
 import Layout from '../../components/Layout';
 import TaskForm from '../../components/TaskForm';
 import TaskList from '../../components/TaskList';
 import { isAuthenticated } from '../../utils/auth';
 
-export default function Tasks() {
-  const [editingTask, setEditingTask] = useState(null);
+export default function TasksPage() {
   const router = useRouter();
+  const [editingTask, setEditingTask] = useState(null);
 
   useEffect(() => {
-    if (!isAuthenticated()) {
+    if (typeof window !== "undefined" && !isAuthenticated()) {
       router.push('/login');
     }
   }, [router]);
 
-  const handleEdit = (task) => {
-    setEditingTask(task);
-  };
-
-  const handleCancelEdit = () => {
-    setEditingTask(null);
-  };
+  const handleEdit = (task) => setEditingTask(task);
+  const handleCancelEdit = () => setEditingTask(null);
 
   return (
     <Layout>
-      <div className="tasks-page">
-        <TaskForm editingTask={editingTask} onCancel={handleCancelEdit} />
-        <TaskList onEdit={handleEdit} />
-      </div>
+      <TaskForm editingTask={editingTask} onCancel={handleCancelEdit} />
+      <TaskList onEdit={handleEdit} />
     </Layout>
   );
 }
