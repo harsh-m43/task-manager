13c5b2290b2f1dcc262efa35d9f53e0ad24ea771
Harsh Mahale <mahale22bec43@iiitkottayam.ac.in>
2025-08-06 02:40:03 +0530

    Initial commit

--- app/login/page.js
+++ app/login/page.js
@@ -2,14 +2,14 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import LoginForm from '../../components/LoginForm';
+import LoginForm from '../../components/LoginForm';  // Make sure this is also client component if it uses hooks
 import { isAuthenticated } from '../../utils/auth';
 
 export default function Login() {
   const router = useRouter();
-
+  
   useEffect(() => {
-    if (isAuthenticated()) {
+    if (typeof window !== 'undefined' && isAuthenticated()) {
       router.push('/tasks');
     }
   }, [router]);
